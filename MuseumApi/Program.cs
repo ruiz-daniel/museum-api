db4519979b3a0d0cfd7fecaf5e40c5bae12e69e8
DRGWeiss <ruizd1710@gmail.com>
2023-01-02 14:37:14 -0500

    first commit: create api

---
+++ MuseumApi/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using MuseumApi.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MuseumContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
